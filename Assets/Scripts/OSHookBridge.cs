67259a0ff9ee42d8d6e57a3dfa56e3d213829512
Kolapo Oni <kolapooni@Kolapo-MacBook-Air.local>
2016-04-11 15:41:29 +0100

    first commit

---
+++ Assets/Scripts/OSHookBridge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+class OSHookBridge{
+	
+#if UNITY_ANDROID
+
+	public static void shareIntent(string text){
+		AndroidJavaClass ajc = new AndroidJavaClass("com.kaypee.oshook.Bridge");
+		ajc.CallStatic ("shareIntent",text);
+	}
+
+#endif
+
+}
---
+++ Assets/Scripts/PluginControllerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PluginControllerScript : MonoBehaviour {
+	private RuntimePlatform platform = Application.platform;
+    public string shareText;
+
+    public void Share() {
+		if(platform == RuntimePlatform.Android){
+			OSHookBridge.shareIntent (shareText);
+		}
+       
+    }
+
+	//edit this section for sharing to twitter
+	public void ShareToTwitter(){
+		const string Address = "http://twitter.com/intent/tweet";
+		string text = "this minimal plugin was created by @Kolapo_";
+		string lang = "en";
+		//string related = "@kolapo_";
+		Application.OpenURL (Address +"?text=" + WWW.EscapeURL(text) + "&amp;lang=" + WWW.EscapeURL(lang));
+		//Application.OpenURL (Address +"?text=" + WWW.EscapeURL(text) + "&amp;related=" + WWW.EscapeURL(related) + "&amp;lang=" + WWW.EscapeURL(lang));
+	}
+
+}
